d102d83d6d46fb3252ea60ddfd121685f9e6e5cc
周崇名 <chongmingzhou@163.com>
2019-12-19 10:34:30 +0800

    Commit q1 a1

--- src/io/mczeno/leetcode/q1/TwoNumSum.java
+++ src/io/mczeno/leetcode/q1/TwoNumSum.java
@@ -1,5 +1,9 @@
 package io.mczeno.leetcode.q1;
 
+import java.util.AbstractMap;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * TwoNumSum
  *
@@ -7,12 +11,26 @@ package io.mczeno.leetcode.q1;
  */
 public class TwoNumSum {
 
+    private static final int[] ARRAY = {1, 32, 89, 7, 45};
+    private static final int TARGET = 96;
+
     public static void main(String[] args) {
-        int[] array = {1, 3, 5, 7, 4};
-
-
-
-
+        Map<Integer, Map.Entry<Integer, Integer>> sumIndexMap = convertToSumIndexMap(ARRAY);
+        Map.Entry<Integer, Integer> indexEntry = sumIndexMap.get(TARGET);
+        System.out.println("[" + indexEntry.getKey() + ", " + indexEntry.getValue() + "]");
     }
+
+    private static Map<Integer, Map.Entry<Integer, Integer>> convertToSumIndexMap(int[] array) {
+        Map<Integer, Map.Entry<Integer, Integer>> result = new HashMap<>();
+
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                result.put(array[i] + array[j], new AbstractMap.SimpleImmutableEntry<>(i, j));
+            }
+        }
+
+        return result;
+    }
+
 
 }
