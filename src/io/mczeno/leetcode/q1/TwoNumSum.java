987dea26f7d560bd64add139b7d2c58883bd76c3
周崇名 <chongmingzhou@163.com>
2019-12-19 10:11:35 +0800

    [init] first commit

---
+++ src/io/mczeno/leetcode/q1/TwoNumSum.java
@@ -0,0 +1,18 @@
+package io.mczeno.leetcode.q1;
+
+/**
+ * TwoNumSum
+ *
+ * @author Chongming Zhou
+ */
+public class TwoNumSum {
+
+    public static void main(String[] args) {
+        int[] array = {1, 3, 5, 7, 4};
+
+
+
+
+    }
+
+}
